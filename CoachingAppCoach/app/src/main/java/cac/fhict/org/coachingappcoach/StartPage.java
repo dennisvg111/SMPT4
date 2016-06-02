d3c7d1b6f51eae4eb2db2e264cc51ad13e216242
teveve <t.vervelde@student.fontys.nl>
2016-06-02 11:42:43 +0200

    The AppCouchResult screen is done, AppCouchResult screen added.

--- CoachingAppCoach/app/src/main/java/cac/fhict/org/coachingappcoach/StartPage.java
+++ CoachingAppCoach/app/src/main/java/cac/fhict/org/coachingappcoach/StartPage.java
@@ -14,7 +14,7 @@ public class StartPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_coach_start);
+        setContentView(R.layout.activity_app_coach_question);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }

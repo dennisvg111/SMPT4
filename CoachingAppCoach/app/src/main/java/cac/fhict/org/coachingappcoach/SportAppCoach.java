c67ccf656c70d0a91226b61ae9dce3817e5a4794
christje <chrissieboyvangils@hotmail.com>
2016-06-09 14:20:03 +0200

    App Finder Question, Engish

--- CoachingAppCoach/app/src/main/java/cac/fhict/org/coachingappcoach/SportAppCoach.java
+++ CoachingAppCoach/app/src/main/java/cac/fhict/org/coachingappcoach/SportAppCoach.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Sport_App_Coach extends AppCompatActivity {
+public class SportAppCoach extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

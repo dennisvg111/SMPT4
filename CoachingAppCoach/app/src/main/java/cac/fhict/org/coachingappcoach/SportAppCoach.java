90466a66ea5e9b5ccafe6ac89a62a657818136fb
BartMemelink <b.memelink@student.fontys.nl>
2016-06-10 14:04:36 +0200

    SportAppCoach links

--- CoachingAppCoach/app/src/main/java/cac/fhict/org/coachingappcoach/SportAppCoach.java
+++ CoachingAppCoach/app/src/main/java/cac/fhict/org/coachingappcoach/SportAppCoach.java
@@ -1,13 +1,18 @@
 package cac.fhict.org.coachingappcoach;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class SportAppCoach extends AppCompatActivity {
+
+    Button btnAppfinder;
+    Button btnAppCoach;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,28 @@ public class SportAppCoach extends AppCompatActivity {
         setContentView(R.layout.activity_sport__app__coach);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        btnAppCoach = (Button)findViewById(R.id.button2);
+        btnAppfinder = (Button)findViewById(R.id.button);
+
+        btnAppfinder.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SportAppCoach.this, AppFinderStart.class);
+                SportAppCoach.this.startActivity(intent);
+            }
+        });
+
+        btnAppCoach.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(SportAppCoach.this, AppCoachStart.class);
+                SportAppCoach.this.startActivity(intent);
+            }
+        });
     }
+
 
 }
